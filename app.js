2ae1780494371be92ead5f1b18955dbde31d864b
Divine <tediadjene@gmail.com>
2023-11-12 19:25:59 +0100

    Complete switch quiz

--- app.js
+++ app.js
@@ -2,13 +2,15 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-
+const path = require('path');
 const app = express();
 
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
+
+app.use(express.static(__dirname + "/public"));
 
 mongoose.connect("mongodb://127.0.0.1:27017/QuizDB")
 
@@ -27,7 +29,6 @@ app.get("/questions", function (req, res) {
     res.render("questions")
 })
 
-
 app.post("/questions", function (req, res) {
     const quiz = new Quiz({
         question: req.body.newQuestion,
@@ -38,14 +39,18 @@ app.post("/questions", function (req, res) {
 })
 
 app.get("/startQuiz", function (req, res) {
+    const quizID = req.params.id
+
     Quiz.find({})
         .then((quiz) => {
             res.render("Started", {
-                savedQuiz: quiz
+                savedQuiz: quiz,
+                currentQuizIndex: 0
             })
         }).catch((error) => {
             console.log(error)
         })
+    
 })
 
 app.listen(4000, function () {
