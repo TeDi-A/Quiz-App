2df2bd19e0a5e50913e6ebe89ea2eaa4f9c00b21
Divine <tediadjene@gmail.com>
2024-02-13 08:20:29 +0100

    Update Quiz App

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ const mongoose = require("mongoose");
 const path = require('path');
 const app = express();
 
-app.set('view engine', 'ejs');
+// app.set('view engine', 'ejs');
+app.set("views", path.join(__dirname, "views"))
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
